3c17bbadcb935e3509c697b0bce40476157b2a9c
priyaihsan <priyaihsan771@gmail.com>
2021-06-22 23:24:44 +0700

    firebase act10

--- app/src/main/java/com/example/firebaseapp/MainActivity.java
+++ app/src/main/java/com/example/firebaseapp/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         bLihat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //besok dilanjud
+                startActivity(LihatBarang.getActIntent(MainActivity.this));
             }
         });
     }
